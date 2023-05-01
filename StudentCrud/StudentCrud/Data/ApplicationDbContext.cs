8de4286363cb53b66f7370a5185d629ab94a3855
Kalpesh7781 <118824729+Kalpesh7781@users.noreply.github.com>
2023-05-02 01:32:12 +0530

    Add files via upload

---
+++ StudentCrud/StudentCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCrud.Models;
+using System.Data.Common;
+
+namespace StudentCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        
+        }
+        
+        public DbSet<Student> Student { get; set; }    
+    }
+}
