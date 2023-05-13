192cdd8e80d0944146f462edb963d19bcbae74e3
Kalpesh7781 <118824729+Kalpesh7781@users.noreply.github.com>
2023-05-13 22:34:00 +0530

    Add files via upload

---
+++ CrudWithApi/CrudWithApi/Model/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudWithApi.Model
+{
+    [Table("Brand")]
+        public class Brand
+        {
+            [Key]
+            public int Id { get; set; }
+            /*  public object ID { get; internal set; }*/
+            public string? Name { get; set; }
+            public string? Category { get; set; }
+
+            public int IsActive { get; set; }
+        }
+     
+}
---
+++ CrudWithApi/CrudWithApi/Model/BrandContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudWithApi.Model
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+        }
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
