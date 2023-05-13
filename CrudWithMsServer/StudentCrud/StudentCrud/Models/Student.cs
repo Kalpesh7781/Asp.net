606045039d15b8419d0b598d251f104052170e58
Kalpesh7781 <118824729+Kalpesh7781@users.noreply.github.com>
2023-05-13 22:12:20 +0530

    Add files via upload

---
+++ CrudWithMsServer/StudentCrud/StudentCrud/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentCrud.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string City { get; set; }
+        public int Mobile { get; set; }
+    }
+}
